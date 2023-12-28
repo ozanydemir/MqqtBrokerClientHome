cd432d7efc1025f854c2e6649fe1b7c0ec7ef0a8
Ozan <oziyukseldemir@gmail.com>
2023-12-29 01:41:31 +0300

    Add project files.

---
+++ MqqtBrokerClientHome/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace MqqtBrokerClientHome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPublish = new System.Windows.Forms.Button();
+            this.txtAlıcı = new System.Windows.Forms.ListBox();
+            this.lblSay = new System.Windows.Forms.Label();
+            this.lbldeneme = new System.Windows.Forms.Label();
+            this.lblMN01 = new System.Windows.Forms.Label();
+            this.timer_MN01 = new System.Windows.Forms.Timer(this.components);
+            this.timer1_MN02 = new System.Windows.Forms.Timer(this.components);
+            this.lblMN02 = new System.Windows.Forms.Label();
+            this.timer_CMA03 = new System.Windows.Forms.Timer(this.components);
+            this.lblCMA03 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnPublish
+            // 
+            this.btnPublish.Location = new System.Drawing.Point(678, 38);
+            this.btnPublish.Name = "btnPublish";
+            this.btnPublish.Size = new System.Drawing.Size(110, 38);
+            this.btnPublish.TabIndex = 1;
+            this.btnPublish.Text = "Publish";
+            this.btnPublish.UseVisualStyleBackColor = true;
+            this.btnPublish.Click += new System.EventHandler(this.btnPublish_Click);
+            // 
+            // txtAlıcı
+            // 
+            this.txtAlıcı.FormattingEnabled = true;
+            this.txtAlıcı.Location = new System.Drawing.Point(5, 330);
+            this.txtAlıcı.Name = "txtAlıcı";
+            this.txtAlıcı.Size = new System.Drawing.Size(783, 108);
+            this.txtAlıcı.TabIndex = 4;
+            // 
+            // lblSay
+            // 
+            this.lblSay.AutoSize = true;
+            this.lblSay.Location = new System.Drawing.Point(12, 228);
+            this.lblSay.Name = "lblSay";
+            this.lblSay.Size = new System.Drawing.Size(35, 13);
+            this.lblSay.TabIndex = 5;
+            this.lblSay.Text = "label1";
+            // 
+            // lbldeneme
+            // 
+            this.lbldeneme.AutoSize = true;
+            this.lbldeneme.Location = new System.Drawing.Point(12, 187);
+            this.lbldeneme.Name = "lbldeneme";
+            this.lbldeneme.Size = new System.Drawing.Size(35, 13);
+            this.lbldeneme.TabIndex = 6;
+            this.lbldeneme.Text = "label1";
+            // 
+            // lblMN01
+            // 
+            this.lblMN01.AutoSize = true;
+            this.lblMN01.Location = new System.Drawing.Point(253, 289);
+            this.lblMN01.Name = "lblMN01";
+            this.lblMN01.Size = new System.Drawing.Size(35, 13);
+            this.lblMN01.TabIndex = 7;
+            this.lblMN01.Text = "label1";
+            // 
+            // timer_MN01
+            // 
+            this.timer_MN01.Tick += new System.EventHandler(this.timer_MN01_Tick);
+            // 
+            // timer1_MN02
+            // 
+            this.timer1_MN02.Tick += new System.EventHandler(this.timer1_MN02_Tick);
+            // 
+            // lblMN02
+            // 
+            this.lblMN02.AutoSize = true;
+            this.lblMN02.Location = new System.Drawing.Point(304, 289);
+            this.lblMN02.Name = "lblMN02";
+            this.lblMN02.Size = new System.Drawing.Size(35, 13);
+            this.lblMN02.TabIndex = 8;
+            this.lblMN02.Text = "label1";
+            // 
+            // timer_CMA03
+            // 
+            this.timer_CMA03.Tick += new System.EventHandler(this.timer_CMA03_Tick);
+            // 
+            // lblCMA03
+            // 
+            this.lblCMA03.AutoSize = true;
+            this.lblCMA03.Location = new System.Drawing.Point(345, 289);
+            this.lblCMA03.Name = "lblCMA03";
+            this.lblCMA03.Size = new System.Drawing.Size(35, 13);
+            this.lblCMA03.TabIndex = 9;
+            this.lblCMA03.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCMA03);
+            this.Controls.Add(this.lblMN02);
+            this.Controls.Add(this.lblMN01);
+            this.Controls.Add(this.lbldeneme);
+            this.Controls.Add(this.lblSay);
+            this.Controls.Add(this.txtAlıcı);
+            this.Controls.Add(this.btnPublish);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPublish;
+        private System.Windows.Forms.ListBox txtAlıcı;
+        private System.Windows.Forms.Label lblSay;
+        private System.Windows.Forms.Label lbldeneme;
+        private System.Windows.Forms.Label lblMN01;
+        private System.Windows.Forms.Timer timer_MN01;
+        private System.Windows.Forms.Timer timer1_MN02;
+        private System.Windows.Forms.Label lblMN02;
+        private System.Windows.Forms.Timer timer_CMA03;
+        private System.Windows.Forms.Label lblCMA03;
+    }
+}
+
---
+++ MqqtBrokerClientHome/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqqtBrokerClientHome
+{
+    public partial class Form1 : Form
+    {
+        MqttClient mqttClient;
+
+        TimeSpan MN01T, MN02T,CMA03T, CMA04T, CMA05T, CMA06T, CMA07T, CMA08T;
+        double MN01S, MN02S, CMA03S, CMA04S, CMA05S, CMA06S, CMA07S, CMA08S = 0;
+
+        Stopwatch MN01K = new Stopwatch();
+        Stopwatch MN02K = new Stopwatch();
+        Stopwatch CMA03K = new Stopwatch();
+        Stopwatch CMA04K = new Stopwatch();
+        Stopwatch CMA05K = new Stopwatch();
+        Stopwatch CMA06K = new Stopwatch();
+        Stopwatch CMA07K = new Stopwatch();
+        Stopwatch CMA08K = new Stopwatch();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                mqttClient = new MqttClient("127.0.0.1");
+                //mqttClient = new MqttClient("193.100.100.105");
+
+                mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+
+                //string[] topics = { "MN01", "MN02", "CMA03", "CMA04", "CMA05", "CMA06", "CMA07", "CMA08" };
+                //byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,
+                //MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE};
+
+                string[] topics = { "test-veris" };
+                byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE };
+                mqttClient.Subscribe(topics, qosLevels);
+                mqttClient.Connect(Guid.NewGuid().ToString());
+
+            });
+
+            timer_MN01.Start();
+            timer1_MN02.Start();
+            timer_CMA03.Start();
+
+        }
+
+        private void MqttClient_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Message);
+            if (e.Topic == "test-veris")
+            {
+                txtAlıcı.Items.Add(message);
+
+                if (message == "On")
+                {
+                    MN01K.Start();
+                }
+
+                if (message == "Off")
+                {
+                    MN01K.Stop();
+                }
+            }
+
+            if (e.Topic == "MN02")
+            {
+                if (message == "On")
+                {
+                    MN02K.Start();
+                }
+
+                if (message == "Off")
+                {
+                    MN02K.Stop();
+                }
+            }
+
+            if (e.Topic == "CMA03")
+            {
+                if (message == "On")
+                {
+                    CMA03K.Start();
+                }
+
+                if (message == "Off")
+                {
+                    CMA03K.Stop();
+                }
+            }
+        }
+
+        private void btnPublish_Click(object sender, EventArgs e)
+        {
+            Task task = Task.Run(() =>
+            {
+                if (mqttClient != null && mqttClient.IsConnected)
+                {
+                    mqttClient.Publish("test-alis", Encoding.UTF8.GetBytes(textBox1.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+                }
+            });
+        }
+
+        private void timer_MN01_Tick(object sender, EventArgs e)
+        {
+            MN01T = MN01K.Elapsed;
+            MN01S = MN01T.TotalMilliseconds;
+
+            MN01S = Math.Floor(MN01S);
+            lblMN01.Text = MN01S.ToString();
+        }
+
+        private void timer1_MN02_Tick(object sender, EventArgs e)
+        {
+            MN02T = MN02K.Elapsed;
+            MN02S = MN02T.TotalMilliseconds;
+
+            MN02S = Math.Floor(MN02S);
+            lblMN02.Text = MN02S.ToString();
+        }
+
+        private void timer_CMA03_Tick(object sender, EventArgs e)
+        {
+            CMA03T = CMA03K.Elapsed;
+            CMA03S = CMA03T.TotalMilliseconds;
+
+            CMA03S = Math.Floor(CMA03S);
+            lblCMA03.Text = CMA03S.ToString();
+        }
+
+
+    }
+}
+
+
+
+
